ce5709e491cb32bb21e3fe8a5fa0f04f987fdb23
ww2luccaww1 <ww2luccaww1@gmail.com>
2025-10-16 19:03:37 -0400

    Update Tampermonkey-code.js

--- Tampermonkey-code.js
+++ Tampermonkey-code.js
@@ -9,16 +9,3 @@
 // @run-at document-end
 // @grant none
 // ==/UserScript==
-var realismGo = null;
-let itv = setInterval(
-function(){
-try{
-if(window.ui && window.flight){
-main();
-clearInterval(itv);}
-}catch(err){}
-}
-,500);
-function main() {
-var realismScript = document.createElement('script'); realismScript.src="https://raw.githack.com/NVB9ALT/Realism-pack./main/main.js";document.body.appendChild(realismScript);realismScript.onload = function(){realismGo()};
-};
